ccd03d804579f483c8737b6f4516569563c28f7e
manishraj2021 <manish.raj_cs19@gla.ac.in>
2022-02-10 23:16:04 +0530

    EndOfFile ke liye class banaya h.

---
+++ src/HackerRankProblem/EndOfFile.java
@@ -0,0 +1,10 @@
+package HackerRankProblem;/*
+ * Author Name: Manish Raj
+ * IDE: IntelliJ IDEA Ultimate Edition
+ * JDK: 16 version
+ * Date: 10-02-2022
+ * Time: 23:07
+ */
+
+public class EndOfFile {
+}
